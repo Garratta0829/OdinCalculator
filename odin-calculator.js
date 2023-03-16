d6948bd14723a56ecb2bca6e490d3d55500d2598
Andrew Garratt <Andrew.GarrattN@outlook.com>
2023-03-15 18:45:10 -0700

    multiple calculations work

--- odin-calculator.js
+++ odin-calculator.js
@@ -3,50 +3,93 @@ const operators = document.querySelectorAll('.operator')
 const bottomScreen = document.querySelector('.bottom-screen')
 const topScreen = document.querySelector('.top-screen')
 const equalButton = document.querySelector('.equal')
-const addButton = document.getElementById('add')
-const subtractButton = document.getElementById('subtract')
-const multiplyButton = document.getElementById('multiply')
-const divideButton = document.getElementById('divide');
+const clearButton = document.querySelector('.clear')
+const decimal = document.querySelector('.decimal')
+// const addButton = document.getElementById('add')
+// const subtractButton = document.getElementById('subtract')
+// const multiplyButton = document.getElementById('multiply')
+// const divideButton = document.getElementById('divide');
 let operand
 let operator1
 let num1 = ''
 let num2 = ''
 let numberString = []
+let currentValue = 0
 // let previousValue
 // let currentValue = 0;
 // let value
 let operatorPressed = false;
+let equalPressed = false;
 
 
 
 operands.forEach(button => button.addEventListener('click', () => {
+//   if(equalPressed === false) {
     if (operatorPressed === false) {
         num1 += button.innerHTML
         bottomScreen.textContent += button.innerHTML
         topScreen.textContent += button.innerHTML
-        console.log(num1)
+        // console.log(num1)
     } else if (operatorPressed === true) {
         bottomScreen.textContent = ''
         num2 += button.innerHTML
-        bottomScreen.textContent += button.innerHTML
-        topScreen.textContent += button.innerHTML
-        console.log(num2)
+        // num1 += button.innerHTML
+        bottomScreen.textContent += button.innerHTML 
+        topScreen.textContent += button.innerHTML 
+        // console.log(num1)
+        // console.log(num2)
     }
+//   } else if (equalPressed === true) {
+//         num1 = currentValue
+//         console.log(num1)
+//         num2 = 0
+//         num2 += button.innerHTML
+//         bottomScreen.textContent += button.innerHTML 
+//         topScreen.textContent += button.innerHTML 
+//   }
+    
     
     
 }))
 
-operators.forEach(operator1 => operator1.addEventListener('click', () => {
+// if operator pressed for second time without hitting equals, num1 + num2 has to be calculated 
+// then stored.
+
+operators.forEach(op => op.addEventListener('click', () => {
     operatorPressed = true
-    operator = operator1.innerHTML
+    // Number(num1 + num2)
+    // console.log(num1)
+    operator = op.innerHTML
     topScreen.textContent += operator
+    console.log(num1)
+    console.log(num2)
+    // if (operator === '+') {
+    //     num1 += bottomScreen.innerHTML
+    //     console.log(num1)
+        // num1 += num2
+        // num2 = ''
+        // console.log(num1)
+        // console.log(num2)
+    }
     
-}))
+))
 
 // operator = operator1.innerHTML
 // topScreen.textContent += operator
 // numberString += value
 // topScreen.innerHTML = 
+
+decimal.addEventListener('click', () => {
+    if (operatorPressed === false) {
+        parseFloat(num1 += decimal.innerHTML)
+        bottomScreen.textContent += decimal.innerHTML
+        topScreen.textContent += decimal.innerHTML
+    } else if (operatorPressed === true) {
+        parseFloat(num2 += decimal.innerHTML)
+        bottomScreen.textContent += decimal.innerHTML
+        topScreen.textContent += decimal.innerHTML
+    }
+})
 
 function operation(){
     
@@ -67,21 +110,30 @@ function operation(){
 // })
 
 equalButton.addEventListener('click', () => {
+    // num1 = parseFloat(num1) 
+    // num2 = parseFloat(num2)
     operate(operator, num1, num2)
-
+    // equalPressed = true
+    num1 = bottomScreen.textContent
+    num2 = ''
+    // num1 = operate(operator, num1, num2)
+    // console.log(operate(operator, num1, num2))
+    // console.log(num1)
+    // console.log(currentValue)
 })
 
-
-function topScreenNumbers(value) {
-    
-    // console.log(number)
-    // topScreen.innerHTML = numberString.join(' ')
-}
+clearButton.addEventListener('click', () => {
+    topScreen.innerHTML = ''
+    bottomScreen.innerHTML = ''
+    num1 = ''
+    num2 = ''
+    operatorPressed = false;
+})
 
 
 function add(num1, num2) {
     // console.log(a + b) 
-    return num1 + num2
+    console.log(num1 + num2)
 }
 
 function divide(num1,num2) {
@@ -96,7 +148,7 @@ function multiply(num1, num2) {
 
 function subtract(num1, num2) {
     // console.log(a - b) 
-    return num1 - num2
+    bottomScreen.innerHTML = num1 - num2
 }
 
 
@@ -107,30 +159,32 @@ function operate(operator, num1, num2) {
     switch (operator) {
         case '+':
             // return num1 + num2;
+            // currentValue += num1 + num2
             bottomScreen.innerHTML = num1 + num2
+            
+            // num1 = ''
+            // num2 = ''
+            // num1 = bottomScreen.textContent
+            // topScreen.textContent += num
+            // console.log(num1)
             break;
         case '-':
-            return subtract(num1, num2);
+            bottomScreen.innerHTML = num1 - num2
+            // return subtract(num1, num2);
             break;
         case '*':
-            return multiply(num1, num2);
+            bottomScreen.innerHTML = num1 * num2
+            // return multiply(num1, num2);
             break;
         case '/':
-            return divide(num1, num2);
+            bottomScreen.innerHTML = num1 / num2
+            // return divide(num1, num2);
             break;
         default:
             console.log('blah blah blah')
     }
     
-    // if (operator === '+') {
-    //     previousValue += currentValue
-    // } else if (operator === '-') {
-    //     subtract(num1, num2)
-    // } else if (operator === '*') {
-    //     multiply(num1, num2)
-    // } else if (operator === '/') {
-    //     divide(num1, num2)
-    // }
+ 
 }
 
 
